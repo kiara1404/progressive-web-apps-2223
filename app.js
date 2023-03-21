438c95119be2ba29567636e409c111cb47c532eb
Kiara <kiarabroekhuizen1@hotmail.com>
2023-03-21 13:32:55 +0100

    saving items started

--- app.js
+++ app.js
@@ -14,20 +14,34 @@ app.set("views", "views");
 // public folder location
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
 // Routing
-app.get("/", getIndex);
-app.get("/scanner", getScanner);
-app.get("/zoeken", getSearch);
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/scanner", (req, res) => {
+  res.render("scanner");
+});
+
+app.get("/zoeken", (req, res) => {
+  res.render("search");
+});
+
+app.post("/bewaren", (req, res) => {
+  const productId = req.body.product_id;
+  console.log(req.body.product_id);
+  res.redirect(`/products/${productId}`);
+});
+
+// search
 app.get("/zoek", async (req, res) => {
-  console.log(req.query);
   const response = await fetch(
     `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${req.query.query}&search_simple=1&action=process&json=1`
   );
   const data = await response.json();
-  console.log(typeof data);
   const cleanProductsArray = data.products.map(item => {
     try {
-      console.log(item._id);
       return {
         id: item._id,
         name: item.brands,
@@ -37,45 +51,28 @@ app.get("/zoek", async (req, res) => {
       console.log(e);
     }
   });
-  console.log("res", cleanProductsArray);
-  res.render("includes/result-list", { data: cleanProductsArray.slice(0, 5) });
+  res.render("results", {
+    data: cleanProductsArray.slice(0, 5),
+    query: req.query.query,
+  });
 });
 
-app.get("/products/:id", getDetailPage);
-
-const API_URL = "https://world.openfoodfacts.org/api/v0/product/";
-
-function server() {
-  console.log(
-    "The server is running succesfully! 🎉 at https://http://localhost:8000/"
-  );
-}
-
-function getIndex(req, res) {
-  res.render("index");
-}
-
-function getSearch(req, res) {
-  res.render("search");
-}
-
-function getScanner(req, res) {
-  res.render("scanner");
-}
-
-// fetch data
-async function getDetailPage(req, res) {
+app.get("/products/:id", async (req, res) => {
+  const API_URL = "https://world.openfoodfacts.org/api/v0/product/";
   try {
     const barcode = req.params.id;
     const response = await fetch(API_URL + barcode + "json");
     const data = await response.json();
-    console.log(data.product);
     res.render("detail", {
       data: data,
     });
   } catch (err) {
     console.log(err);
   }
-}
+});
 
-app.listen(port, server);
+app.listen(port, () => {
+  console.log(
+    "The server is running succesfully! 🎉 at https://http://localhost:8000/"
+  );
+});
