07c01d97d564477ed9409c8f8019841a3197cde0
Kiara <kiarabroekhuizen1@hotmail.com>
2023-06-10 23:45:09 +0200

    100

--- static/js/index.js
+++ static/js/index.js
@@ -36,7 +36,7 @@ if (window.location.pathname === "/scanner") {
 }
 
 // If supported, install service worker.
-if ("serviceWorker" in navigator) {
+if ("serviceWorker" in  navigator) {
   console.log("service worker supported!");
   window.addEventListener("load", () => {
     navigator.serviceWorker
@@ -52,34 +52,3 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-let deferredPrompt;
-let addBtn;
-
-if (document.querySelector(".add-button")) {
-  addBtn = document.querySelector(".add-button");
-
-  window.addEventListener("beforeinstallprompt", e => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = "block";
-
-    addBtn.addEventListener("click", e => {
-      // hide our user interface that shows our A2HS button
-      addBtn.style.display = "none";
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then(choiceResult => {
-        if (choiceResult.outcome === "accepted") {
-          console.log("User accepted the A2HS prompt");
-        } else {
-          console.log("User dismissed the A2HS prompt");
-        }
-        deferredPrompt = null;
-      });
-    });
-  });
-}
--- static/service-worker.js
+++ static/service-worker.js
@@ -2,16 +2,14 @@ const PRE_CACHE = "pre cache";
 //pre cache all the files that you definitly want to have
 const files = [
   "/offline",
-  "/js/index.js",
-  "/styles/index.css",
+  "/js/index.min.js",
+  "/styles/index.min.css",
   "/img/food.webp",
 ];
 
 // Install the service worker.
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
     caches.open(PRE_CACHE).then(cache => {
       self.skipWaiting();
       return cache.addAll(files);
@@ -20,8 +18,7 @@ self.addEventListener("install", event => {
 });
 
 // Activate service worker.
-self.addEventListener("activate", _event => {
-});
+self.addEventListener("activate", _event => {});
 
 // Fetch  service worker.
 self.addEventListener("fetch", event => {
@@ -29,7 +26,6 @@ self.addEventListener("fetch", event => {
 
   // Check if any of the requested files already exists
   if (event.request.method === "GET" && files.includes(url.pathname)) {
-
     event.respondWith(caches.open(PRE_CACHE).then(cache => cache.match(url)));
 
     // Only request the HTML, all the other files are already in the cache.
